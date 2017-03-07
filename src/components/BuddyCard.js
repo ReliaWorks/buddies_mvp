62cb2aa1f1255ee6c3a087d8fb48d836dae5c419
Shireen B <shireen@relia.works>
2017-03-06 23:29:27 -0800

    Implemented vertical photo swiping in the BuddyCard component

--- src/components/BuddyCard.js
+++ src/components/BuddyCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text, Image, View } from 'react-native';
+import Swiper from 'react-native-swiper';
 import { CardItem, Tile } from './common';
 
 const tileIcon = "https://firebasestorage.googleapis.com/v0/b/activities-test-a3871.appspot.com/o/img%2Factivity_icons%2Frunning_exercise.png?alt=media&token=7b691e7a-5831-432a-a146-32dd04dc0984";
@@ -11,10 +12,26 @@ const BuddyCard = (props) => {
   return (
       <View style={{flex: 1}}>
         <View style={containerStyle}>
-          <Image
-            source={{ uri: profileImageURL }}
-            style={imageStyle}
-          />
+          <Swiper horizontal={false}>
+            <View>
+              <Image
+                source={{ uri: profileImageURL }}
+                style={imageStyle}
+              />
+            </View>
+            <View>
+              <Image
+                source={{ uri: 'https://firebasestorage.googleapis.com/v0/b/activities-test-a3871.appspot.com/o/img%2FPeter2.png?alt=media&token=debe4efa-7318-46fb-a47f-18ab9e34e9b1' }}
+                style={imageStyle}
+              />
+            </View>
+            <View>
+              <Image
+                source={{ uri: 'https://firebasestorage.googleapis.com/v0/b/activities-test-a3871.appspot.com/o/img%2FPeter3.JPG?alt=media&token=7844831f-a89b-439b-9e66-0774432d527f' }}
+                style={imageStyle}
+              />
+            </View>
+          </Swiper>
         </View>
         <View style={descriptionContainerStyle}>
           <Text style={textStyle}>{firstName}, {age}</Text>
