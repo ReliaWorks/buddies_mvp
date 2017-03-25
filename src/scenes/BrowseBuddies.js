530d1b0afa5c26f542217df47d14e818fa988c52
Oscar Valdivia <omvaldiv@gmail.com>
2017-03-25 14:17:43 -0500

    Loading from axios

--- src/scenes/BrowseBuddies.js
+++ src/scenes/BrowseBuddies.js
@@ -1,23 +1,43 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import axios from 'axios';
+import { View, Text } from 'react-native';
 import Swiper from 'react-native-swiper';
 import { connect } from 'react-redux';
 import BuddyCard from '../components/buddycard/BuddyCard';
 import { Spinner } from '../components/common';
-import userSampleData from '../components/demo-data/Users.js';
 
 class BrowseBuddies extends Component {
+
+  state = { matches: [] };
+
+  componentWillMount() {
+    axios.get('https://matching-api.appspot.com/match/0du4iTIWosZCGXvMmd0jmUYFoUW2')
+      .then(response => {
+        const keys = Object.keys(response.data);
+        const arr = [];
+
+        keys.forEach((key) => {
+          arr[key] = response.data[key];
+        });
+
+        console.log('arr',arr);
+
+        this.setState({ matches: arr });
+      });
+  }
+
   render() {
-    if(!userSampleData) {
+    if(this.state.matches.length === 0) {
       return (
         <View style={{justifyContent: 'center', alignItems: 'center'}}>
           <Spinner size="large" />
         </View>
       );
     } else {
-      return (
-        <Swiper>
-          {userSampleData.map((buddy, key) => {
+        console.log("got results");
+        return(
+          <Swiper>
+          {this.state.matches.map((buddy, key) => {
             return (
               <View key={key} style={styles.cardStyle}>
                 <BuddyCard
@@ -36,7 +56,7 @@ class BrowseBuddies extends Component {
             );
           })}
         </Swiper>
-      );
+        );
     }
   }
 }
