91f50c22d2e95e0855deece8f3f03b9cfca23db9
Shireen B <shireen@relia.works>
2017-04-07 10:01:25 -0700

    Updated to point to production wavelength firebase and facebook

--- src/config.js
+++ src/config.js
@@ -1,9 +1,11 @@
 export const FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyC5B1L0NfK0WsZYRtVjVUleo6To9aFuDf8',
-  authDomain: 'activities-test-a3871.firebaseapp.com',
-  databaseURL: 'https://activities-test-a3871.firebaseio.com',
-  storageBucket: 'activities-test-a3871.appspot.com',
-  messagingSenderId: '432468217036'
+  apiKey: "AIzaSyB47DxjGQBKb-y0eSPVjJ9kh93_5YzXg5Y",
+  authDomain: "wavelength-d78bb.firebaseapp.com",
+  databaseURL: "https://wavelength-d78bb.firebaseio.com",
+  projectId: "wavelength-d78bb",
+  storageBucket: "wavelength-d78bb.appspot.com",
+  messagingSenderId: "139518548932"
 };
+
 
 export const DEFAULT_PROFILE_PHOTO = 'https://firebasestorage.googleapis.com/v0/b/activities-test-a3871.appspot.com/o/img%2Fdefault-user-image.png?alt=media&token=5a29c303-0a94-4640-a917-9283f1ecdb66';
