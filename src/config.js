74648c3f4cf992ce0141db9f04ba7363e8a474ec
Oscar Valdivia <omvaldiv@gmail.com>
2017-05-30 12:53:01 -0400

    Using google API locally

--- src/config.js
+++ src/config.js
@@ -7,3 +7,5 @@ export const FIREBASE_CONFIG = {
 };
 
 export const SENTRY_DNS = "https://2f81115717434870acf29f260688c226:63241df70957413bb812dcd4ed0b2c7c@sentry.io/169776";
+
+export const MAP_API_KEY = 'AIzaSyACWmDGmgYDEvWuzvjpDn9GYjrafCZOSKw';
