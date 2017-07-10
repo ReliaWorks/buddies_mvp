8a6720ca2a5957cda85be01e43df1bdaae6097c9
Shireen Brathwaite <sbrathwaite@Shireens-MacBook-Pro.local>
2017-07-10 11:11:49 -0700

    Android updates to get app running

--- index.android.js
+++ index.android.js
@@ -1,9 +1,9 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 
-import { Sentry } from 'react-native-sentry';
+//import { Sentry } from 'react-native-sentry';
 
-Sentry.config("YOUR_DSN_HERE").install();
+//Sentry.config("YOUR_DSN_HERE").install();
 
 
 AppRegistry.registerComponent('buddies_v3', () => App);
